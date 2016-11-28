7159ee130fb6b7f4883ff5f2c121733cb8f42096
dpgc11 <iamyogeshtelang@gmail.com>
2016-11-28 09:48:05 +0530

    finished 3 exercises

--- src/Warmup/CompareTheTriplets.java
+++ src/Warmup/CompareTheTriplets.java
@@ -3,8 +3,34 @@ package Warmup;
 public class CompareTheTriplets {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			
+		Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int points1=0,points2=0;
+        
+        if (a0 > b0) {
+        	points1++;
+        } else if (a0 < b0) {
+        	points2++;
+        }
+        
+        if (a1 > b1) {
+        	points1++;
+        } else if (a1 < b1) {
+        	points2++;
+        }
+        
+        if (a2 > b2) {
+        	points1++;
+        } else if (a2 < b2) {
+        	points2++;
+        }
+        
+        System.out.println(points1 + " " + points2);			
 	}
 
 }
