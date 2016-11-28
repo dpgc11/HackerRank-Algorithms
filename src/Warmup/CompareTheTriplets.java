741fd87b1d3d433a6eae6cc9f6480be53cd8642d
dpgc11 <iamyogeshtelang@gmail.com>
2016-11-28 09:32:05 +0530

    finished 2 exercises

---
+++ src/Warmup/CompareTheTriplets.java
@@ -0,0 +1,10 @@
+package Warmup;
+
+public class CompareTheTriplets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Warmup/SolveMeFirst.java
@@ -0,0 +1,24 @@
+package Warmup;
+
+import java.util.Scanner;
+
+public class SolveMeFirst {
+	
+	static int solveMeFirst(int a, int b) {
+        // Hint: Type return a+b; below 
+       return a + b;
+  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+	}
+
+}
