bfd1f84d6c75099113bd85d3df8c170fd9b6ad01
dpgc11 <iamyogeshtelang@gmail.com>
2016-11-29 15:38:42 +0530

    warmup complete

--- src/warmup/CircularArrayRotation.java
+++ src/warmup/CircularArrayRotation.java
@@ -6,42 +6,21 @@ public class CircularArrayRotation {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int k = in.nextInt();
-		int q = in.nextInt();
-		int[] a = new int[n];
-		for (int a_i = 0; a_i < n; a_i++) {
-			a[a_i] = in.nextInt();
-		}
-		for (int a0 = 0; a0 < q; a0++) {
-			int m = in.nextInt();
-		}
+		int N = in.nextInt();
+		int K = in.nextInt();
+		int Q = in.nextInt();
+		int rot = K % N;
+		int[] arr = new int[N];
 
-		printArray(a);
-		System.out.println();
-		a = rotateArray(a, k);
-		printArray(a);
-	}
+		for (int i = 0; i < N; i++)
+			arr[i] = in.nextInt();
 
-	public static int[] rotateArray(int[] array, int rotations) {
-		int length = array.length;
-		int[] rotatedArray = new int[length];
-
-		for (int j = 0; j < rotations; j++) {
-			rotatedArray[0] = array[length - 1];
-
-			for (int i = 1; i < length; i++) {
-				rotatedArray[i] = array[i - 1];
-			}
-		}
-
-		return rotatedArray;
-	}
-
-	public static void printArray(int[] array) {
-		for (int j = 0; j < array.length; j++) {
-			System.out.println(array[j]);
+		for (int i = 0; i < Q; i++) {
+			int idx = in.nextInt();
+			if (idx - rot >= 0)
+				System.out.println(arr[idx - rot]);
+			else
+				System.out.println(arr[idx - rot + arr.length]);
 		}
 	}
-
 }
