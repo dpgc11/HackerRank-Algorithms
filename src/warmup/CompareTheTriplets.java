e79bedda26992df5c438b4efe2a45162afe21d4c
yogeshTelang <yogesh@lifinglabs.com>
2018-01-29 15:37:35 +0530

    refactor: CompareTheTriplets

--- src/warmup/CompareTheTriplets.java
+++ src/warmup/CompareTheTriplets.java
@@ -4,35 +4,43 @@ import java.util.Scanner;
 
 public class CompareTheTriplets {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-        int a0 = in.nextInt();
-        int a1 = in.nextInt();
-        int a2 = in.nextInt();
-        int b0 = in.nextInt();
-        int b1 = in.nextInt();
-        int b2 = in.nextInt();
-        int points1=0,points2=0;
-        
-        if (a0 > b0) {
-        	points1++;
-        } else if (a0 < b0) {
-        	points2++;
-        }
-        
-        if (a1 > b1) {
-        	points1++;
-        } else if (a1 < b1) {
-        	points2++;
-        }
-        
-        if (a2 > b2) {
-        	points1++;
-        } else if (a2 < b2) {
-        	points2++;
-        }
-        
-        System.out.println(points1 + " " + points2);			
-	}
+	 static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+	        // Complete this function
+		 int[] result = new int[2];
+		 if (a0 > b0) {
+			 result[0] += 1;
+		 } else if (a0 < b0) {
+			 result[1] += 1;
+		 } 
+		 if (a1 > b1) {
+			 result[0] += 1;
+		 } else if (a1 < b1) {
+			 result[1] += 1;
+		 } 
+		 if (a2 > b2) {
+			 result[0] += 1;
+		 } else if (a2 < b2) {
+			 result[1] += 1;
+		 } 
+		 return result;
+		 
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int a0 = in.nextInt();
+	        int a1 = in.nextInt();
+	        int a2 = in.nextInt();
+	        int b0 = in.nextInt();
+	        int b1 = in.nextInt();
+	        int b2 = in.nextInt();
+	        int[] result = solve(a0, a1, a2, b0, b1, b2);
+	        for (int i = 0; i < result.length; i++) {
+	            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+	        }
+	        System.out.println("");
+	        
+
+	    }
 
 }
