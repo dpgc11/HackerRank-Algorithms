fcc393456c63f97813b70be7666fd5aa4cc84be4
yogeshTelang <yogesh@lifinglabs.com>
2018-02-06 20:04:12 +0530

    feat: ApplesAndOranges

--- src/implementation/ApplesAndOranges.java
+++ src/implementation/ApplesAndOranges.java
@@ -4,37 +4,43 @@ import java.util.Scanner;
 
 public class ApplesAndOranges {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        // Complete this function
+		int appleCount = 0;
+		int orangeCount = 0;
+		
+		for (int i : apples) {
+			if ((i + a) >= s && (i + a) <= t) {
+				appleCount++;
+			}
+		}
+		for (int i : oranges) {
+			if ((i + b) >= s && (i + b) <= t) {
+				orangeCount++;
+			}
+		}
+		
+		System.out.println(appleCount + "\n" + orangeCount);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         int s = in.nextInt();
         int t = in.nextInt();
         int a = in.nextInt();
         int b = in.nextInt();
         int m = in.nextInt();
         int n = in.nextInt();
-        int appleCount = 0;
-        int orangeCount = 0;
-        
-        for (int i = 0; i < m; i++) {
-        	int temp = in.nextInt();
-        	int pos = a + temp;
-        	if (pos >= s && pos <= t) {
-        		appleCount++;
-        	}
+        int[] apple = new int[m];
+        for(int apple_i = 0; apple_i < m; apple_i++){
+            apple[apple_i] = in.nextInt();
         }
-        for (int i = 0; i < n; i++) {
-        	int temp = in.nextInt();
-        	int pos = b + temp;
-        	if (pos >= s && pos <= t) {
-        		orangeCount++;
-        	}
+        int[] orange = new int[n];
+        for(int orange_i = 0; orange_i < n; orange_i++){
+            orange[orange_i] = in.nextInt();
         }
-        
-        System.out.println(appleCount);
-        System.out.println(orangeCount);
-        
-        
-        
-	}
+        countApplesAndOranges(s, t, a, b, apple, orange);
+        in.close();
+    }
 
 }
