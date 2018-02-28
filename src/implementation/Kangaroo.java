5fc4eb34861fe52a90907c3c9f35036abed9f47f
yogeshTelang <yogesh@lifinglabs.com>
2018-02-28 22:39:52 +0530

    feat: Kangaroo

--- src/implementation/Kangaroo.java
+++ src/implementation/Kangaroo.java
@@ -4,13 +4,30 @@ import java.util.Scanner;
 
 public class Kangaroo {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int x1 = in.nextInt();
-		int v1 = in.nextInt();
-		int x2 = in.nextInt();
-		int v2 = in.nextInt();
-		System.out.println((v2 < v1 && (x2 - x1) % (v2 - v1) == 0) ? "YES" : "NO");
-	}
+	static String kangaroo(int x1, int v1, int x2, int v2) {
+        // Complete this function
+		String result = "NO";
+		if (v1 > v2) {
+			while (x1 <= x2) {
+				x1 = x1 + v1;
+				x2 = x2 + v2;
+				if (x1 == x2) {
+					result = "YES";
+				}
+			}
+		}
+		
+		return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        String result = kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
+    }
 
 }
