3baaad0aa293f039730b8f2060bc085070820776
yogeshTelang <yogesh@lifinglabs.com>
2018-03-20 15:00:29 +0530

    feat: AngryProfessor

--- src/implementation/AngryProfessor.java
+++ src/implementation/AngryProfessor.java
@@ -4,24 +4,36 @@ import java.util.Scanner;
 
 public class AngryProfessor {
 
-	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
-		int numOfTestCases = reader.nextInt();
-		while (numOfTestCases-- > 0) {
-			int numOfStudents = reader.nextInt();
-			int numOfStudentsRequired = reader.nextInt();
-			int count = 0;
-			while (numOfStudents-- > 0) {
-				int arrivalTime = reader.nextInt();
-				if (arrivalTime <= 0) {
-					count++;
-				}
+	static String angryProfessor(int k, int[] a) {
+        // Complete this function
+		String result = "YES";
+		int count = 0;
+		for (int i : a) {
+			if (i <= 0) {
+				count++;
 			}
-			if (count >= numOfStudentsRequired)
-				System.out.println("NO");
-			else
-				System.out.println("YES");
 		}
-	}
+		if (count >= k) {
+			result = "NO";
+		}
+		
+		return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int[] a = new int[n];
+            for(int a_i = 0; a_i < n; a_i++){
+                a[a_i] = in.nextInt();
+            }
+            String result = angryProfessor(k, a);
+            System.out.println(result);
+        }
+        in.close();
+    }
 
 }
