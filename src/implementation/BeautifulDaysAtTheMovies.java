55af19fcfca2e25b0490bbe26edf2e9ecceb5df6
yogeshTelang <yogesh@lifinglabs.com>
2018-03-21 16:31:43 +0530

    feat: BeautifulDaysAtTheMovies

--- src/implementation/BeautifulDaysAtTheMovies.java
+++ src/implementation/BeautifulDaysAtTheMovies.java
@@ -4,32 +4,43 @@ import java.util.Scanner;
 
 public class BeautifulDaysAtTheMovies {
 
-	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
-		int startingDay = reader.nextInt();
-		int endingDay = reader.nextInt();
-		int divisibleBy = reader.nextInt();
-		reader.close();
-		int count = 0;
-
-		for (int i = startingDay; i <= endingDay; i++) {
-
-			if ((Math.abs(i - revNum(i))) % divisibleBy == 0) {
-				count++;
+	static int beautifulDays(int i, int j, int k) {
+        // Complete this function
+		int result = 0;
+		for (int num = i; num <= j; num++) {
+			System.out.println("diff: " + calculateDifference(num));
+			System.out.println("exp: " + calculateDifference(num) % k);
+			if ((calculateDifference(num) % k) == 0) {
+				result++;
 			}
 		}
-		System.out.println(count);
-	}
+		return result;
+    }
 
-	public static int revNum(int num) {
-		int rem = 0, rev = 0;
-		while (num != 0) {
-			rem = num % 10;
-			rev *= 10;
-			rev += rem;
-			num /= 10;
-		}
-		return rev;
-	}
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();
+        int j = in.nextInt();
+        int k = in.nextInt();
+        int result = beautifulDays(i, j, k);
+        System.out.println(result);
+        in.close();
+    }
+    
+    public static int reverseNum(int num) {
+    	String tempSum = "";
+    	int rem = 0;
+    	while (num > 0) {
+    		rem = num % 10;
+    		tempSum += String.valueOf(rem);
+    		num /= 10;
+    	}
+    	return Integer.parseInt(tempSum);
+    }
+    
+    public static int calculateDifference(int i) {
+    	int result = (Math.abs(i - reverseNum(i)));
+    	return result;
+    }
 
 }
